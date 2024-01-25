393f1de128055f27d0688ec17dacde1b226b461c
mochafreddo <corc411@gmail.com>
2024-01-25 17:24:25 +0900

    feat: enhance users/enter API for user upsert functionality
    - Expanded the `handler` function in `pages/api/users/enter.tsx` to handle both email and phone-based user identification.
    - Implemented `upsert` logic using Prisma client to either find or create a user based on the provided email or phone number.
    - Removed commented-out conditional logic for finding or creating users, as `upsert` method efficiently handles these scenarios.
    - Added logging to track user creation or retrieval in the database.
    - Ensured the API returns a 200 status code after successful user handling.

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,41 @@
+import client from "@/libs/server/client";
 import withHandler from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { email, phone } = req.body;
+  const payload = email ? { email } : { phone: +phone };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      ...payload,
+      name: "Anonymous",
+    },
+    update: {},
+  });
+  console.log(user);
+  /* if (email) {
+    user = await client.user.findUnique({ where: { email } });
+    if (user) console.log("found it.");
+    if (!user) {
+      console.log("Did not find. Will create.");
+      user = await client.user.create({ data: { email, name: "Anonymous" } });
+    }
+    console.log(user);
+  }
+  if (phone) {
+    user = await client.user.findUnique({ where: { phone: +phone } });
+    if (user) console.log("found it.");
+    if (!user) {
+      console.log("Did not find. Will create.");
+      user = await client.user.create({
+        data: { name: "Anonymous", phone: +phone },
+      });
+    }
+    console.log(user);
+  } */
   return res.status(200).end();
 }
 
