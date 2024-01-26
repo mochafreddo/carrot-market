028b17ab5677c2423ee1b8881789a183d0a020d1
mochafreddo <corc411@gmail.com>
2024-01-26 09:28:11 +0900

    feat: add response type definition to withHandler for standardized API responses refactor: update users/enter API to use ResponseType and enhance validation
    - Enhanced `withHandler` in `libs/server/withHandler.ts` by introducing `ResponseType` interface for consistent API response structure.
    - Updated `users/enter` API handler in `pages/api/users/enter.tsx` to utilize the new `ResponseType`, providing a standardized response format.
    - Improved user data validation in the API handler, now checking for the presence of either `email` or `phone` and returning a 400 status code if neither is provided.
    - Simplified the API response to return a JSON object with an 'ok' status, adhering to the new `ResponseType` structure.
    - Removed commented-out legacy code and console logs for a cleaner and more maintainable codebase.

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,10 +1,14 @@
 import client from "@/libs/server/client";
-import withHandler from "@/libs/server/withHandler";
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>,
+) {
   const { email, phone } = req.body;
-  const user = email ? { email } : { phone: +phone };
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await client.token.create({
     data: {
@@ -22,28 +26,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       },
     },
   });
-  console.log(token);
-  /* if (email) {
-    user = await client.user.findUnique({ where: { email } });
-    if (user) console.log("found it.");
-    if (!user) {
-      console.log("Did not find. Will create.");
-      user = await client.user.create({ data: { email, name: "Anonymous" } });
-    }
-    console.log(user);
-  }
-  if (phone) {
-    user = await client.user.findUnique({ where: { phone: +phone } });
-    if (user) console.log("found it.");
-    if (!user) {
-      console.log("Did not find. Will create.");
-      user = await client.user.create({
-        data: { name: "Anonymous", phone: +phone },
-      });
-    }
-    console.log(user);
-  } */
-  return res.status(200).end();
+  return res.json({ ok: true });
 }
 
 export default withHandler("POST", handler);
